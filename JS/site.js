177163de6ac7fae4a1615fcf7b9d0613c331b9ac
Nathan, Probably <145495899+nathanprobably@users.noreply.github.com>
2023-10-16 16:46:51 -0400

    Initial commit

---
+++ JS/site.js
@@ -0,0 +1,14 @@
+function displayMessage() {
+    //get the text out of our <input> and assign it to a variable
+    let msg = document.getElementById('message').value;
+
+
+
+    Swal.fire(
+        {
+            backdrop: false,
+            title: 'APP NAME',
+            text: msg, //<-- replace this with that variable
+        }
+    );
+}
