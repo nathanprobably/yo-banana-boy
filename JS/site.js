f03160fb6a5193496d2321e9b99e0a44f2cabda7
Nathan, Probably <nathanlautzenheiser@gmail.com>
2023-10-17 09:26:07 -0400

    built app

--- JS/site.js
+++ JS/site.js
@@ -1,14 +1,48 @@
-function displayMessage() {
-    //get the text out of our <input> and assign it to a variable
-    let msg = document.getElementById('message').value;
+//steps to complete...
+//get the message out of the input
+//get the message to reverse
+//check if the message is a palindrome
+//display the message either confirming or denying if it is a palindrome
 
+function getValues() {
+    let input = document.getElementById('userInput').value
 
-
-    Swal.fire(
-        {
+    if (input.length == 0) {
+        Swal.fire({
+            icon: 'error',
             backdrop: false,
-            title: 'APP NAME',
-            text: msg, //<-- replace this with that variable
+            title: 'Hmm...',
+            text: 'You played yourself! Try entering some text into the input box...'
+        })
+    } else {
+       let results = checkForPalindrome(input);
+
+       displayResults(results);
+    }
+}
+
+function checkForPalindrome(input) {
+    let output = '';
+
+    for(let i=input.length - 1; i >= 0; i--) {
+        let letter = input[i];
+        output += letter
+    }
+
+    if(output.toLowerCase().split('').join('').replace(/[^a-zA-z0-9 ]/g, '') !=input.toLowerCase().split('').join('').replace(/[^a-zA-z0-9 ]/g, '')) {
+        return [false, output]
+    } else {
+        return [true, output];
         }
-    );
+}
+
+//display the reversed message
+function displayResults(results) {
+    if(results[0] == true) {
+        document.getElementById('input').textContent = `You're message is a palindrome: ${results[1]}`;
+        document.getElementById('alert').classList.remove('invisible');
+    } else {
+        document.getElementById('input').textContent = `Sorry, you're message is not a palindrome: ${results[1]}`;
+        document.getElementById('alert').classList.remove('invisible');
+    }
 }
